15376d3b33b0ba9423367eb082d0e2eb3802e5de
widcardw <lui747353367@qq.com>
2022-11-26 10:59:59 +0800

    add avatar

--- compress.ts
+++ compress.ts
@@ -1,7 +1,7 @@
 import { batSharp } from 'bat-sharp'
 
 batSharp({
-  inputArr: ['./assets/**/*.webp'],
+  inputArr: ['./source/**/*.jpg'],
   format: 'webp', // png jpeg webp avif and so on
   outputPath: './compressed',
   outputConfig: { // docs: https://sharp.pixelplumbing.com/api-output#png
